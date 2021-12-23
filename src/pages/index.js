271d10fd756e3a89c2f735bf29230ccb20e43f91
Adoliin <ben.meft0@gmail.com>
2021-12-23 20:21:41 +0100

    to test opengraph og:image

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Features from '../components/sections/Features'
 import IllustrationFeatures from '../components/sections/IllustrationFeatures'
 import Download from '../components/sections/Download'
 
-//import OwlSvg from '../assets/owl-only.svg'
+import HomeThumbnail from '../images/bitsai-og-square-200.png'
 
 import '../css/layout.css'
 
@@ -23,20 +23,24 @@ const Index = () => (
     <Helmet>
       <title>BitSai - Bitcoin Wallet</title>‍
       <meta name="description" content={DESCRIPTION}/>
+
       <meta property="og:title" content={TITLE} />
       <meta property="og:description" content={DESCRIPTION} />
-      {/*<meta property="og:image" content={OwlSvg}/>*/}
+      <meta property="og:image" content={`https://bitsai.co${HomeThumbnail}`}/>
+      <meta property="og:image:alt" content="bitsai-home-page"/>
+      {/*<meta property="og:image:width" content="809"/>
+      <meta property="og:image:height" content="809"/>*/}
       <meta property="og:url" content="https://bitsai.co" />
       <meta property="og:site_name" content={TITLE} />
       <meta property="og:locale" content="en_US" />
       <meta property="og:type" content="Landing page" />
 
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={TITLE} />
       <meta name="twitter:description" content={DESCRIPTION} />
-      {/*<meta name="twitter:card" content={OwlSvg} />*/}
       <meta name="twitter:site" content="Firas" />
       <meta name="twitter:creator" content="Firas" />
-      {/*<meta name="twitter:image" content={OwlSvg} />*/}
+      <meta name="twitter:image" content={`https://bitsai.co${HomeThumbnail}`} />
 
       <script
         data-goatcounter="https://bitsai.goatcounter.com/count"
